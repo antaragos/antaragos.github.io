ad13dc8b65b08ec02232c9199aadab6b0a32af5d
antaragos <43261782+antaragos@users.noreply.github.com>
2024-12-06 16:18:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 >
-<!DOCTYPE html>
+<html>
 <head>
     <title>My Test Site</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,3 +11,4 @@
     <h1><?php echo "It's a PHP"?></h1>
     <a href="https://192.168.64.3:8043/redock/">Redirect</a>
 </body>
+</html>
