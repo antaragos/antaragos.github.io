1b4f071fc15b19c6b9f64e566de3fa14a68c9539
antaragos <43261782+antaragos@users.noreply.github.com>
2024-12-06 15:35:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>My Test Site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=https://192.168.64.3:8043/redock/" />
+</head>
+<body>It's mine
+    <p>My first PHP script!</p>
+    <h1><?php echo "It's a PHP"?></h1>
+    <a href="https://192.168.64.3:8043/redock/">Redirect</a>
+</body>
